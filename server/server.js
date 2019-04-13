db6d7f3c51ff65d0a8ddff3b4bb9faa8b79b1bda
Konsta Purtsi <kovipu@utu.fi>
2019-04-13 20:02:08 +0300

    Added backend for music metadata

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const WebSocket = require('ws');
+const express = require('express');
 
 const wss = new WebSocket.Server({ port: 3030 });
+const app = express();
+
+const PORT = process.env.PORT || 3031;
+
+let currentSong = 'No song playing';
 
 console.log('Listening for socket connections on port 3030');
 
@@ -17,4 +23,19 @@ wss.on('connection', function connection(ws) {
       });
     }
   });
+});
+
+app.post('newsong', (req, res) => {
+  const song = req.body;
+
+  if (!song || typeof song !== 'string') {
+    return res.sendStatus(400)
+  }
+
+  currentSong = song;
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening for song metadata updates on port ${PORT}`)
 });
